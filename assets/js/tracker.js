9dd2be6c3ac56dda1be03c04709af7368f8a3ccf
johnc365 <johnrcaldwell123@gmail.com>
2024-10-02 11:30:38 -0500

    added javascript

---
+++ assets/js/tracker.js
@@ -0,0 +1,16 @@
+// function displayFormData(formData) {
+//     document.getElementById('monthlyIncome').textContent = `$${formData.income}`;
+//     document.getElementById('housing').textContent = `$${formData.expenses.rent}`;
+//     document.getElementById('utilities').textContent = `$${formData.expenses.utilities}`;
+//     document.getElementById('groceries').textContent = `$${formData.expenses.groceries}`;
+//     document.getElementById('gasTransportation').textContent = `$${formData.expenses.gas}`;
+//     document.getElementById('car-insurance').textContent = `$${formData.expenses.insurance.carInsurance}`;
+//     document.getElementById('home-insurance').textContent = `$${formData.expenses.insurance.homeInsurance}`;
+//     document.getElementById('life-insurance').textContent = `$${formData.expenses.insurance.lifeInsurance}`;
+//     document.getElementById('subscribtions').textContent = `$${formData.expenses.subscribe}`;
+//     document.getElementById('other').textContent = `$${formData.expenses.other}`;
+
+//     document.getElementById('goalOne').textContent = `${formData.goals.goalName}`;
+//     document.getElementById('goal-amount').textContent = `$${formData.goals.goalAmount}`;
+//     document.getElementById('goal-months').textContent = `${formData.goals.months}`;
+// }
